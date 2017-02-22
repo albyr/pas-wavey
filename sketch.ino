96659358911558e6a575ce73baac68df706b2d10
Alby <alby@bleary-id.co.uk>
2017-02-22 10:07:30 +0000

    Initial fading code

--- sketch.ino
+++ sketch.ino
@@ -13,9 +13,9 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 	for (int currentPin = 0; currentPin < ledCount; currentPin++) {
-    	digitalWrite(ledPins[currentPin], HIGH);
-		delay(1000);
-		digitalWrite(ledPins[currentPin], LOW);
-		delay(1000);
+		for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+			analogWrite(ledPins[currentPin], fadeValue);
+			delay(30);
+  		}
 	}
 }

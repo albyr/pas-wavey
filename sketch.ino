0ee93892ee3b4961f8239aaff69547072729d605
Alby <alby@bleary-id.co.uk>
2017-02-22 19:32:47 +0000

    Add debugging setup

--- sketch.ino
+++ sketch.ino
@@ -4,10 +4,13 @@ int ledCount = sizeof(ledPins)/sizeof(int);
 
 // Setup
 void setup() {
+	// Open serial port for debugging
+	Serial.begin(9600);
 	// Initialise all relevant LED pins as outputs
 	for (int currentPin = 0; currentPin < ledCount; currentPin++) {
 		pinMode(ledPins[currentPin], OUTPUT);
 	}
+	Serial.println("Program Start");
 }
 
 // the loop routine runs over and over again forever:
